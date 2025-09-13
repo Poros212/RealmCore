e0444c81cc146734ecfb8a09a5a1301ccea74f25
Poros212 <120608558+Poros212@users.noreply.github.com>
2025-09-12 23:44:09 -0400

    Seperated battle manager and battle options

---
+++ RealmCore.Logic/Battle/BattleOptions.cs
@@ -0,0 +1,113 @@
+﻿using RealmCore.Logic.AI;
+using RealmCore.Logic.SnapShots;
+using RealmCore.Logic.Tiles;
+using RealmCore.Logic.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RealmCore.Logic.Interfaces;
+
+namespace RealmCore.Logic.Battle
+{
+    public class BattleOptions
+    {
+        public BattleContext CTX { get; set; }
+        public IBattlefield? BattlefieldImplementation { get; set; }
+
+        public BattleOptions(BattleContext ctx, IBattlefield battlefield)
+        {
+            CTX = ctx;
+            BattlefieldImplementation = battlefield;
+        }
+
+        public bool ActionMenu(Player actor)
+        {
+            //SetActiveActor(actor);
+
+            int choice = BattlefieldImplementation.DisplayBattleMenu();
+
+            switch (choice)
+            {
+                case 1:
+                    PlayerTurnMovement(actor);
+                    return true;
+
+                case 2:
+                    //battlemenu
+                    return true;
+
+                case 3:
+                    return false;
+
+                default:
+                    return false;
+            }
+        }
+
+        public void EnemyActions(Player actor)
+        {
+            EnemyTurnMovement(actor);
+        }
+
+        //public void SetActiveActor(Player actor)
+        //{
+        //    ActiveActor = actor;
+        //    CTX.ActiveActorId = actor.ActorId;
+        //}
+
+        public DefaultAi InitiateEnemyAi()
+        {
+            var ctxSnap = SnapshotFactoryBattle.CreateSnapshotBattleContext(CTX);
+            return new DefaultAi(ctxSnap);
+        }
+
+        public void PlayerTurnMovement(Player actor)
+        {
+            //SetActiveActor(actor);
+
+            while (true)
+            {
+                string? errorMessage = CTX.BattleField.MoveActor(actor, BattlefieldImplementation.TryMove(actor)).ErrorMessage;
+
+                if (errorMessage != null && errorMessage != "exit")
+                {
+                    BattlefieldImplementation.ShowError(errorMessage);
+                }
+                else if (errorMessage == "exit")
+                {
+                    break;
+                }
+            }
+        }
+
+        public void EnemyTurnMovement(Player actor)
+        {
+            List<Tile> tilesWalked = new List<Tile>();
+
+            while (true)
+            {
+                //SetActiveActor(actor);
+
+                DtoValidationResult<string> action = InitiateEnemyAi().TakeTurn();
+
+                if (actor.ChosenCharacter.CurrentMovementPoints > 0 && action.Value != "exit")
+                {
+                    Tile tempTile = CTX.BattleField.TileArray[actor.XCoordinate, actor.YCoordinate];
+                    tilesWalked.Add(tempTile);
+                    CTX.BattleField.MoveActor(actor, action.Value);
+                    CTX.BattleField.TileArray[tempTile.XAxis, tempTile.YAxis].Terrain.IsWalkable = false;
+                }
+                else
+                {
+                    foreach (var tile in tilesWalked)
+                    {
+                        CTX.BattleField.TileArray[tile.XAxis, tile.YAxis].Terrain.IsWalkable = true;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+}
--- RealmCore.Logic/Managers/BattleManager.cs
+++ RealmCore.Logic/Managers/BattleManager.cs
@@ -3,6 +3,7 @@ using RealmCore.Logic.Battle;
 using RealmCore.Logic.Interfaces;
 using RealmCore.Logic.SnapShots;
 using RealmCore.Logic.Tiles;
+using RealmCore.Logic.Validations;
 
 namespace RealmCore.Logic.Managers
 {
@@ -12,11 +13,13 @@ namespace RealmCore.Logic.Managers
         public IBattlefield? BattlefieldImplementation { get; set; }
         public List<Player> TurnOrder { get; set; } = new List<Player>();
         public Player ActiveActor { get; set; }
+        public BattleOptions BattleOptions { get; set; }
 
         public BattleManager(BattleContext ctx, IBattlefield? battlefieldUi)
         {
             CTX = ctx;
             BattlefieldImplementation = battlefieldUi;
+            //BattleOptions = new BattleOptions(CTX, BattlefieldImplementation);
         }
 
         public void StartBattle()
@@ -42,7 +45,7 @@ namespace RealmCore.Logic.Managers
         {
             foreach (var actor in TurnOrder)
             {
-                if (actor.typeFlag == "player")
+                if (actor.TypeFlag == "player")
                 {
                     int y = 7;
                     CTX.BattleField.PlaceActor(actor, (CTX.BattleField.Height - 1), y);
@@ -57,58 +60,66 @@ namespace RealmCore.Logic.Managers
             }
         }
 
-        public void TurnMovement(Player actor)
+        //public void TurnMovement(Player actor, DtoValidationResult<string>? action)
+        //{
+        //    ActiveActor = actor;
+        //    CTX.ActiveActorId = actor.ActorId;
+        //    SetActiveActor(actor);
+        //    if (actor.TypeFlag == "player" && actor.IsAlive)
+        //    {
+        //        while (true)
+        //        {
+        //            string? errorMessage = CTX.BattleField.MoveActor(ActiveActor, BattlefieldImplementation.TryMove(ActiveActor)).ErrorMessage;
+
+        //            if (errorMessage != null && errorMessage != "exit")
+        //            {
+        //                BattlefieldImplementation.ShowError(errorMessage);
+        //            }
+        //            else if (errorMessage == "exit")
+        //            {
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    else if (actor.TypeFlag == "enemy" && actor.IsAlive)
+        //    {
+        //        List<Tile> tilesWalked = new List<Tile>();
+
+        //        while (true)
+        //        {
+        //            //var ctxSnap = SnapshotFactoryBattle.CreateSnapshotBattleContext(CTX);
+        //            //DefaultAi defaultAi = new DefaultAi(ctxSnap);
+        //            //var action = defaultAi.TakeTurn();
+        //            SetActiveActor(actor);
+        //            InitiateEnemyAi();
+        //            if (ActiveActor.ChosenCharacter.CurrentMovementPoints > 0 && action.Value != "exit")
+        //            {
+        //                Tile tempTile = CTX.BattleField.TileArray[ActiveActor.XCoordinate, ActiveActor.YCoordinate];
+        //                tilesWalked.Add(tempTile);
+        //                CTX.BattleField.MoveActor(ActiveActor, action.Value);
+        //                CTX.BattleField.TileArray[tempTile.XAxis, tempTile.YAxis].Terrain.IsWalkable = false;
+        //            }
+        //            else
+        //            {
+        //                foreach (var tile in tilesWalked)
+        //                {
+        //                    CTX.BattleField.TileArray[tile.XAxis, tile.YAxis].Terrain.IsWalkable = true;
+        //                }
+        //                break;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("error");
+        //        Console.ReadKey();
+        //    }
+        //}
+
+        public void SetActiveActor(Player actor)
         {
             ActiveActor = actor;
             CTX.ActiveActorId = actor.ActorId;
-
-            if (actor.TypeFlag == "player" && actor.IsAlive)
-            {
-                while (true)
-                {
-                    string? errorMessage = CTX.BattleField.MoveActor(ActiveActor, BattlefieldImplementation.TryMove(ActiveActor)).ErrorMessage;
-
-                    if (errorMessage != null && errorMessage != "exit")
-                    {
-                        BattlefieldImplementation.ShowError(errorMessage);
-                    }
-                    else if (errorMessage == "exit")
-                    {
-                        break;
-                    }
-                }
-            }
-            else if (actor.TypeFlag == "enemy" && actor.IsAlive)
-            {
-                List<Tile> tilesWalked = new List<Tile>();
-
-                while (true)
-                {
-                    var ctxSnap = SnapshotFactoryBattle.CreateSnapshotBattleContext(CTX);
-                    DefaultAi defaultAi = new DefaultAi(ctxSnap);
-                    var action = defaultAi.TakeTurn();
-                    if (ActiveActor.ChosenCharacter.CurrentMovementPoints > 0 && action.Value != "exit")
-                    {
-                        Tile tempTile = CTX.BattleField.TileArray[ActiveActor.XCoordinate, ActiveActor.YCoordinate];
-                        tilesWalked.Add(tempTile);
-                        CTX.BattleField.MoveActor(ActiveActor, action.Value);
-                        CTX.BattleField.TileArray[tempTile.XAxis, tempTile.YAxis].Terrain.IsWalkable = false;
-                    }
-                    else
-                    {
-                        foreach (var tile in tilesWalked)
-                        {
-                            CTX.BattleField.TileArray[tile.XAxis, tile.YAxis].Terrain.IsWalkable = true;
-                        }
-                        break;
-                    }
-                }
-            }
-            else
-            {
-                Console.WriteLine("error");
-                Console.ReadKey();
-            }
         }
 
         public void BattleFlow()
@@ -123,12 +134,16 @@ namespace RealmCore.Logic.Managers
 
                         while (exitCheck)
                         {
-                            exitCheck = ActionMenu(actor);
+                            SetActiveActor(actor);
+                            BattleOptions = new BattleOptions(CTX, BattlefieldImplementation);
+                            exitCheck = BattleOptions.ActionMenu(actor);
                         }
                     }
                     else if (actor.TypeFlag == "enemy" && actor.IsAlive)
                     {
-                        TurnMovement(actor);
+                        SetActiveActor(actor);
+                        BattleOptions = new BattleOptions(CTX, BattlefieldImplementation);
+                        BattleOptions.EnemyActions(actor);
                     }
                     else
                     {
@@ -143,29 +158,92 @@ namespace RealmCore.Logic.Managers
             }
         }
 
-        public bool ActionMenu(Player actor)
-        {
-            ActiveActor = actor;
-            CTX.ActiveActorId = actor.ActorId;
+        //public bool ActionMenu(Player actor)
+        //{
+        //    SetActiveActor(actor);
 
-            int choice = BattlefieldImplementation.DisplayBattleMenu();
+        //    int choice = BattlefieldImplementation.DisplayBattleMenu();
 
-            switch (choice)
-            {
-                case 1:
-                    TurnMovement(actor);
-                    return true;
+        //    switch (choice)
+        //    {
+        //        case 1:
+        //            PlayerTurnMovement(actor);
+        //            return true;
 
-                case 2:
-                    //battlemenu
-                    return true;
+        //        case 2:
+        //            //battlemenu
+        //            return true;
 
-                case 3:
-                    return false;
+        //        case 3:
+        //            return false;
 
-                default:
-                    return false;
-            }
-        }
+        //        default:
+        //            return false;
+        //    }
+        //}
+
+        //public void EnemyActions(Player actor)
+        //{
+        //    EnemyTurnMovement(actor);
+        //}
+
+        //public void SetActiveActor(Player actor)
+        //{
+        //    ActiveActor = actor;
+        //    CTX.ActiveActorId = actor.ActorId;
+        //}
+
+        //public DefaultAi InitiateEnemyAi()
+        //{
+        //    var ctxSnap = SnapshotFactoryBattle.CreateSnapshotBattleContext(CTX);
+        //    return new DefaultAi(ctxSnap);
+        //}
+
+        //public void PlayerTurnMovement(Player actor)
+        //{
+        //    SetActiveActor(actor);
+
+        //    while (true)
+        //    {
+        //        string? errorMessage = CTX.BattleField.MoveActor(ActiveActor, BattlefieldImplementation.TryMove(ActiveActor)).ErrorMessage;
+
+        //        if (errorMessage != null && errorMessage != "exit")
+        //        {
+        //            BattlefieldImplementation.ShowError(errorMessage);
+        //        }
+        //        else if (errorMessage == "exit")
+        //        {
+        //            break;
+        //        }
+        //    }
+        //}
+
+        //public void EnemyTurnMovement(Player actor)
+        //{
+        //    List<Tile> tilesWalked = new List<Tile>();
+
+        //    while (true)
+        //    {
+        //        SetActiveActor(actor);
+
+        //        DtoValidationResult<string> action = InitiateEnemyAi().TakeTurn();
+
+        //        if (ActiveActor.ChosenCharacter.CurrentMovementPoints > 0 && action.Value != "exit")
+        //        {
+        //            Tile tempTile = CTX.BattleField.TileArray[ActiveActor.XCoordinate, ActiveActor.YCoordinate];
+        //            tilesWalked.Add(tempTile);
+        //            CTX.BattleField.MoveActor(ActiveActor, action.Value);
+        //            CTX.BattleField.TileArray[tempTile.XAxis, tempTile.YAxis].Terrain.IsWalkable = false;
+        //        }
+        //        else
+        //        {
+        //            foreach (var tile in tilesWalked)
+        //            {
+        //                CTX.BattleField.TileArray[tile.XAxis, tile.YAxis].Terrain.IsWalkable = true;
+        //            }
+        //            break;
+        //        }
+        //    }
+        //}
     }
 }
